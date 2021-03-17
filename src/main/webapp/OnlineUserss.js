3900df7749737289ce6b3518a0a2d76dafe4fa83
MKSherbini <mksherbini@gmail.com>
2021-03-17 08:52:16 +0200

    move to workers

--- src/main/webapp/OnlineUserss.js
+++ src/main/webapp/OnlineUserss.js
@@ -1,111 +1,46 @@
 var onlineListContainer2 = document.querySelector("#onlineUsers");
 var messagesContainer = document.querySelector("#messagesContainer");
-var eventSource = new EventSource("SSEChatView");
-eventSource.onopen =openConnection;
-eventSource.onmessage= receiveSSE;
-eventSource.addEventListener("updateMessages",receiveMessage);
 var wSocket;
 
 function connectWS() {
-    wSocket= new WebSocket(((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + "/chatApp/echo");
+    wSocket = new WebSocket(((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + "/chatApp/echo");
 }
 
 $(document).ready(function () {
     connectWS();
+    startWorker();
     console.log("connecting echo")
 })
 
-function receiveMessage (event){
-    console.log("++++"+event.data)
-    var msg = JSON.parse(event.data);
-    msg.forEach(function (e){
-        let img = "https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg";
-        if (e.gender == "Female")
-            img = "https://data.whicdn.com/images/295658437/original.jpg";
+function receiveMessage(html) {
+    messagesContainer.innerHTML += html;
+}
 
-        if (e.orientation == 'Right') {
+function receiveUsers(html) {
+    onlineListContainer2.innerHTML = html;
+}
 
-            messagesContainer.innerHTML += '\n' +
-                '                    <div class="d-flex justify-content-end mb-4">\n' +
-                '                        <div class="msg_cotainer_send">\n' +
-                e.content +
-                '                            <span class="msg_time_send">' + e.date + '</span>\n' +
-                '                        </div>\n' +
-                '                        <div class="img_cont_msg">\n' +
-                '                            <img id="UserPhoto" src="'+img+'" class="rounded-circle user_img_msg">\n' +
-                '                              <label class="sender">' + e.sender + '</label>' +
-                '                        </div>\n' +
-                '                    </div>\n'
-            console.log('send' + e.sender)
-        } else {
-            messagesContainer.innerHTML += '   <div class="d-flex justify-content-start mb-4">\n' +
-                '                        <div class="img_cont_msg">\n' +
-                '                            <img src="'+img+'" class="rounded-circle user_img_msg">\n' +
-                '                              <label class="sender">' + e.sender + '</label>' +
-                '                        </div>\n' +
-                '                        <div class="msg_cotainer">\n' +
-                e.content +
-                '                            <span class="msg_time">' + e.date + '</span>\n' +
-                '                        </div>\n' +
-                '                   </div>\n'
-            console.log('receive' + e.sender)
+function startWorker() {
+    if (typeof (Worker) !== "undefined") {
+        if (typeof (w) == "undefined") {
+            w = new Worker("workerForSSE.js");
         }
-
-    })
-
-
-
+        w.onmessage = function (event) {
+            let html = event.data;
+            console.log(html);
+            if (html.startsWith("<li")) {
+                receiveUsers(html);
+            } else {
+                receiveMessage(html);
+            }
+        };
+    } else {
+        alert("Die, your browser does not support Web Workers...");
+    }
 }
 
-eventSource.addEventListener("updateOnlineUsers",function (event){
-    console.log("Received new Users list")
-    var msg = JSON.parse(event.data);
-    onlineListContainer2.innerHTML = "";
-    msg.forEach(function (e) {
-        console.log("###"+e.gender)
-        if (e.gender == 'Male') {
-            onlineListContainer2.innerHTML +=
-                '                    <li class="active">' +
-                '                        <div class="d-flex bd-highlight">' +
-                '                            <div class="img_cont">' +
-                '                                <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img">\n' +
-                '                                <span class="online_icon"></span>' +
-                '                            </div>\n' +
-                '                            <div class="user_info">' +
-                '                                <span>' + e.name + '</span>' +
-                '                                <p>' + e.name + ' is online</p>' +
-                '                            </div>' +
-                '                        </div>' +
-                '                    </li>' +
-                '                    '
-        } else {
-            onlineListContainer2.innerHTML +=
-                '                    <li class="active">' +
-                '                        <div class="d-flex bd-highlight">' +
-                '                            <div class="img_cont">' +
-                '                                <img src="https://data.whicdn.com/images/295658437/original.jpg" class="rounded-circle user_img">\n' +
-                '                                <span class="online_icon"></span>' +
-                '                            </div>\n' +
-                '                            <div class="user_info">' +
-                '                                <span>' + e.name + '</span>' +
-                '                                <p>' + e.name + ' is online</p>' +
-                '                            </div>' +
-                '                        </div>' +
-                '                    </li>' +
-                '                    '
-        }
-    })
-})
-
-function openConnection(){
-    console.log("Connection opened")
-}
-function receiveSSE (event){
-    console.log(event.data + "From default Event")
-}
-
-$("#textBox").on('keypress',function(e) {
-    if(e.which == 13) {
+$("#textBox").on('keypress', function (e) {
+    if (e.which == 13) {
         sendMessage();
     }
 });
--- src/main/webapp/workerForSSE.js
+++ src/main/webapp/workerForSSE.js
@@ -1,46 +1,95 @@
 var eventSource = new EventSource("SSEChatView");
-eventSource.onopen =openConnection;
-eventSource.onmessage= receiveSSE;
-eventSource.addEventListener("updateMessages",receiveMessage);
-eventSource.addEventListener("updateOnlineUsers",updateUsers);
-function updateUsers(event){
+eventSource.onopen = openConnection;
+eventSource.onmessage = receiveSSE;
+eventSource.addEventListener("updateMessages", receiveMessage);
+
+function receiveMessage(event) {
+    console.log("++++" + event.data)
+    var msg = JSON.parse(event.data);
+    let retHtml = "";
+    msg.forEach(function (e) {
+        let img = "https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg";
+        if (e.gender == "Female")
+            img = "https://data.whicdn.com/images/295658437/original.jpg";
+
+        if (e.orientation == 'Right') {
+
+            retHtml += '\n' +
+                '                    <div class="d-flex justify-content-end mb-4">\n' +
+                '                        <div class="msg_cotainer_send">\n' +
+                e.content +
+                '                            <span class="msg_time_send">' + e.date + '</span>\n' +
+                '                        </div>\n' +
+                '                        <div class="img_cont_msg">\n' +
+                '                            <img id="UserPhoto" src="' + img + '" class="rounded-circle user_img_msg">\n' +
+                '                              <label class="sender">' + e.sender + '</label>' +
+                '                        </div>\n' +
+                '                    </div>\n'
+            console.log('send' + e.sender)
+        } else {
+            retHtml += '   <div class="d-flex justify-content-start mb-4">\n' +
+                '                        <div class="img_cont_msg">\n' +
+                '                            <img src="' + img + '" class="rounded-circle user_img_msg">\n' +
+                '                              <label class="sender">' + e.sender + '</label>' +
+                '                        </div>\n' +
+                '                        <div class="msg_cotainer">\n' +
+                e.content +
+                '                            <span class="msg_time">' + e.date + '</span>\n' +
+                '                        </div>\n' +
+                '                   </div>\n'
+            console.log('receive' + e.sender)
+        }
+    })
+    postMessage(retHtml);
+}
+
+eventSource.addEventListener("updateOnlineUsers", function (event) {
     console.log("Received new Users list")
     var msg = JSON.parse(event.data);
+    let retHtml = "";
+    msg.forEach(function (e) {
+        console.log("###" + e.gender)
+        if (e.gender == 'Male') {
+            retHtml +=
+                '<li class="active">' +
+                '                        <div class="d-flex bd-highlight">' +
+                '                            <div class="img_cont">' +
+                '                                <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" class="rounded-circle user_img">\n' +
+                '                                <span class="online_icon"></span>' +
+                '                            </div>\n' +
+                '                            <div class="user_info">' +
+                '                                <span>' + e.name + '</span>' +
+                '                                <p>' + e.name + ' is online</p>' +
+                '                            </div>' +
+                '                        </div>' +
+                '                    </li>' +
+                '                    '
+        } else {
+            retHtml +=
+                '<li class="active">' +
+                '                        <div class="d-flex bd-highlight">' +
+                '                            <div class="img_cont">' +
+                '                                <img src="https://data.whicdn.com/images/295658437/original.jpg" class="rounded-circle user_img">\n' +
+                '                                <span class="online_icon"></span>' +
+                '                            </div>\n' +
+                '                            <div class="user_info">' +
+                '                                <span>' + e.name + '</span>' +
+                '                                <p>' + e.name + ' is online</p>' +
+                '                            </div>' +
+                '                        </div>' +
+                '                    </li>' +
+                '                    '
+        }
+    })
+    postMessage(retHtml);
+});
+
+function openConnection() {
+    console.log("Connection opened")
 }
 
-this.onmessage=function (event){
-    var msg = JSON.parse(event.data);
-    let img = "https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg";
-    if (msg.gender == "Female")
-        img = "https://data.whicdn.com/images/295658437/original.jpg";
-    var result;
-    if (msg.orientation == 'Right') {
-
-        result= '\n' +
-            '                    <div class="d-flex justify-content-end mb-4">\n' +
-            '                        <div class="msg_cotainer_send">\n' +
-            msg.content +
-            '                            <span class="msg_time_send">' + msg.date + '</span>\n' +
-            '                        </div>\n' +
-            '                        <div class="img_cont_msg">\n' +
-            '                            <img id="UserPhoto" src="'+img+'" class="rounded-circle user_img_msg">\n' +
-            '                              <label class="sender">' + msg.sender + '</label>' +
-            '                        </div>\n' +
-            '                    </div>\n'
-        console.log('send' + msg.sender)
-    } else {
-       result= '   <div class="d-flex justify-content-start mb-4">\n' +
-            '                        <div class="img_cont_msg">\n' +
-            '                            <img src="'+img+'" class="rounded-circle user_img_msg">\n' +
-            '                              <label class="sender">' + msg.sender + '</label>' +
-            '                        </div>\n' +
-            '                        <div class="msg_cotainer">\n' +
-            msg.content +
-            '                            <span class="msg_time">' + msg.date + '</span>\n' +
-            '                        </div>\n' +
-            '                   </div>\n'
-        console.log('receive' + msg.sender)
-    }
-
-        postMessage(result);
+function receiveSSE(event) {
+    console.log(event.data + "From default Event")
 }
+
+
